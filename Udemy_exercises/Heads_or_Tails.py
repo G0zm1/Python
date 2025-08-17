da60efa4c3dd742edcb90e30e3153fa015bc30c6
Matthias Kümmel <mkuemmel26@gmail.com>
2025-08-17 08:57:09 +0200

    Update Heads_or_Tails.py
    Fixed multiple typos

--- Udemy_exercises/Heads_or_Tails.py
+++ Udemy_exercises/Heads_or_Tails.py
@@ -1,8 +1,7 @@
 import random
 
-random_head_or_tails = random.randint(a:0, b:1)
-if random_heads_or_tails == 0:
-  print("Heads)
+random_head_or_tails = random.randint(0,1)
+if random_head_or_tails == 0:
+    print("Heads")
 else:
-  print("Tails")
-  
+    print("Tails")
